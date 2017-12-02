a6b4c2f32c9f7efc05de0985ec7de95991cce9f4
bluppfisk <sander.vandemoortel@gmail.com>
2017-12-02 11:33:26 +0800

    add autofollow.JS

--- README.md
+++ README.md
@@ -2,5 +2,8 @@
 Deepblu API tools
 
 
-## Autoliker.py
-Logs you in with a set of accounts defined in LOGINS and automatically likes every post, beginning with the most recent one, until MAX_LIKES is reached or a previous like is found.
+## autoliker.py
+Logs you in with a set of accounts defined in **LOGINS** and automatically likes every post, beginning with the most recent one, until **MAX_LIKES** is reached or an earlier like is found.
+
+## autofollow.js
+Console JS script to copy automatically add a series of Deepblu users in **userIds** to the following list of a logged-in account with token **authToken**.
---
+++ autofollow.js
@@ -0,0 +1,30 @@
+var userIds = "" // userids separated by a comma
+var authToken = "" // fill in your auth token
+ 
+var userIdArray = userIds.split(",");
+var index = 0;
+function doSomething(index) {
+    if (index === userIdArray.length) {
+        return;
+    }
+    var http = new XMLHttpRequest();
+    var url = "https://prod.tritondive.co/apis/follow/v0/follow";
+    http.open("POST", url, true);
+ 
+    http.setRequestHeader("Content-type", "application/json; charset=utf-8");
+    http.setRequestHeader("Authorization", authToken);
+    http.setRequestHeader("Accept-Language", "en");
+     
+    http.onreadystatechange = function() {
+        if(http.readyState == 4 && http.status == 200) {
+            console.log(http.responseText);
+        }
+    }
+    http.send(JSON.stringify({userId:userIdArray[index]}));
+      
+    setTimeout(function () {
+        doSomething(index+1);
+    }, 1000);
+}
+ 
+doSomething(0);
