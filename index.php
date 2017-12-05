eb01891f0198b2df3d453c762737acc01eae74a7
bluppfisk <sander.vandemoortel@gmail.com>
2017-12-06 00:51:50 +0800

    nicify web wrapper

--- index.php
+++ index.php
@@ -1,58 +1,76 @@
-<html>
+<!doctype html>
+<html lang="en">
 <head>
+	<title>Deepblu Backup Tool</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<meta property="description" content="Backup your Deepblu and COSMIQ dive logs and keep a local copy in UDDF format.">
 	<meta property="og:title" content="Deepblu Backup Tool">
 	<meta property="og:description" content="Backup your Deepblu and COSMIQ dive logs and keep a local copy in UDDF format.">
 	<meta property="og:image" content="http://worldofnonging.com/deepblu-tools/images/imported_into_subsurface.jpg">
 	<meta property="og:type" content="website">
-	<meta property="og:url" content="http://worldofnonging.com/deepblu-tools">
+	<meta property="og:url" content="http://worldofnonging.com/deepblu-tools/">
 	<meta name="twitter:card" content="summary_large_image">
 </head>
 <body>
-	<h1>Deepblu Backup Tool</h1>
-	<span>by Sander Van de Moortel (<a href="mailto:sander.vandemoortel@gmail.com">sander.vandemoortel@gmail.com</a>)</span>
-	<ul>
-		<li>Keep a local backup of your dive logs uploaded to Deepblu.</li>
-		<li>Download uploaded COSMIQ or COSMIQ<sup>+</sup> dive logs onto your computer.</li>
-		<li>Exported data is in <a href="http://uddf.org">UDDF format.</a></li>
-	</ul>
+	<main role="main">
+		<div class="jumbotron">
+			<div class="container">
+				<h1 class="display-4">Deepblu Backup Tool</h1>
+				<p class="lead">Backup your Deepblu and COSMIQ dive logs and keep a local copy in UDDF format</p>
+				<?php
+					if ($_POST['submit'] && !empty($_POST['user'])) {
+				        $user = $_POST['user'];
+				        $password = $_POST['password'];
+				        $command = escapeshellcmd('./backupdives.py '.$user.' '.$password);
+						$result=explode(",", exec('/usr/bin/python3 backupdives.py '.$user.' '.$password));
+						if($result[0]!=='0') {
+							printf("Sometheeng ees wrong, officeur. Error message: %s", $result[1]);
+						} else {
+							printf("<p>That worked! Here's your backup:</p><a role='button' class='btn btn-success' href='done/%s'>Download now</a>", $result[1]);
+						}
+				?>
+				<a role='button' class='btn btn-secondary' href='./'>Again?</a>
+			</div>
+		</div>
+		<div class="container">
+			<h2>What's next?</h2>
+			<ul>
+				<li>Click the button to download your backup in UDDF format.</li>
+				<li>Import this file into your favourite divelog software, such as <a href="https://subsurface-divelog.org">Subsurface</a>.</li>
 
-	<h2>Ready?</h2>
-	<span>Enter your Deepblu username (email address) and password below.</span>
-	<ul>
-		<li>If you signed up through Facebook, just enter your user id instead.</li>
-		<li>To find your user id, visiting your profile page on Deepblu, look at the address bar, and copy the text between <code>/user/</code> and <code>/profile/</code>
-		<li><strong>I will not store your username and password</strong>, but you'll have to take my word for it.</li>
-		<li>If you don't believe me, just enter your user id or download the <a href="https://github.com/bluppfisk/deepblu-tools">Deepblu Backup Tool source code from Github</a> and run it yourself.</li>
-	</ul>
-<form method="POST" action="index.php">
-<input type="text" name="user" placeholder="Deepblu userID or email" />
-<input type="password" name="password" placeholder="password" />
-<input type="submit" name="submit" value="Get backup" />
-</form>
-	<?php
-	if ($_POST['submit'] && !empty($_POST['user']) && isset($_POST['password'])) {
-	        $user = $_POST['user'];
-	        $password = $_POST['password'];
-	        $command = escapeshellcmd('./backupdives.py '.$user.' '.$password);
-	        echo('<h2>Result!</h2><div class="result">');
-			$result=explode(",", exec('/usr/bin/python3 backupdives.py '.$user.' '.$password));
-			if($result[0]!=='0') {
-				printf("<span>Sometheeng ees wrong, officeur. Error message: %s</span>", $result[1]);
+				<li><strong>Before you go...</strong> This took a few days of my life to develop. Consider supporting me with a <a href="https://www.paypal.me/sandervdm?locale.x=en_US&country.x=DE">small donation through PayPal</a>.</li>
+			</ul>
+		</div>
+		<?php
 			} else {
-				printf("<span>That worked! Here's your backup: <a href='done/%s'>%s</a></span>",$result[1], $result[1]);
+				?>
+				<p>
+					<form method="POST" action="index.php">
+						<input type="text" name="user" placeholder="Deepblu userID or email" />
+						<input type="password" name="password" placeholder="Password or blank" />
+						<input type="submit" name="submit" value="Get backup" class="btn btn-primary" />
+					</form>
+				</p>
+			</div>
+		</div>
+		<div class="container">
+			<h2>Instructions</h2>
+			<p>Enter your Deepblu username (email address) and password below.</p>
+			<ul>
+				<li>If you signed up through Facebook, just enter your user id instead.</li>
+				<li>To find your user id, visit your profile page on Deepblu, look at the address bar, and copy the text between <code>/user/</code> and <code>/profile/</code>
+				<li><strong>I will not store your username and password</strong>, but you'll have to take my word for it.</li>
+				<li>If you don't trust me, enter only your user id or download <a href="https://github.com/bluppfisk/deepblu-tools">Deepblu Backup Tool source code</a> and run it yourself.</li>
+			</ul>
+			<?php
 			}
 			?>
-				<h2>What's next</h2>
-				<ul>
-					<li>Click the link to download your backup in UDDF format.</li>
-					<li>Import this file into your favourite divelog software (I like <a href="https://subsurface-divelog.org">Subsurface</a>)</li>
-
-					<li><strong>Before you go...</strong> This took a few days of my life to develop. Consider supporting me with a <a href="https://www.paypal.me/sandervdm?locale.x=en_US&country.x=DE">small donation through PayPal</a>.</li>
-
-			<?php
-	        echo('</div>');
-	}
-	?>
+		</div>
+	</main>
+	<footer class="container">
+		<p class="small">Questions to <a href="mailto:sander.vandemoortel@gmail.com">sander.vandemoortel@gmail.com</a> | <a href="https://github.com/bluppfisk/deepblu-tools">View source code at Github</a> | <a href="https://www.paypal.me/sandervdm?locale.x=en_US&country.x=DE"><img src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_SM.gif"/></a></p>
+	</footer>
 </body>
 </html>
