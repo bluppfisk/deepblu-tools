7c12584ca634ddb2fd7b023b955ce417931ea90c
Sander Van de Moortel <sander.vandemoortel@gmail.com>
2022-10-02 12:52:00 +0200

    fix for unexisting divespots

--- src/deepblu_tools/models/deepblu/dive_spot.py
+++ src/deepblu_tools/models/deepblu/dive_spot.py
@@ -5,7 +5,11 @@ from deepblu_tools.models import deepblu as dm
 # A dive location
 class DiveSpot:
     def __init__(self, divespot: dict):
-        self.id = "deepblu_spot_" + divespot.get("_id")
+        try:
+            self.id = "deepblu_spot_" + divespot.get("_id")
+        except TypeError:
+            self.id = None
+
         self.name = divespot.get("divespot")
         self.lat = divespot.get("gpsLocation", {}).get("lat")
         self.lon = divespot.get("gpsLocation", {}).get("lng")
